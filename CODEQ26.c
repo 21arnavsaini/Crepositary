af0cfaa8adad5dfb1af9974f3615852690fd62cf
REPOSITARY <21arnavsaini@gmail.com>
2025-09-24 23:10:29 +0530

    Q26

---
+++ CODEQ26.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n); // Read input value
+
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", i); // Print numbers from 1 to n
+    }
+
+    return 0;
+}
