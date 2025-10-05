93567338f916810d9b90a337638c58418ed98f39
REPOSITARY <21arnavsaini@gmail.com>
2025-10-05 23:27:39 +0530

    Q36

---
+++ CODEQ36.c
@@ -0,0 +1,20 @@
+//Q36: Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+
+int findHCF(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    int hcf = findHCF(num1, num2);
+    printf("%d\n", hcf);
+    return 0;
+}
