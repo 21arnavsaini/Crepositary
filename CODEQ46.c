35190092fd489d0a1a4cc03da77ec2bb447a41e7
REPOSITARY <21arnavsaini@gmail.com>
2025-10-09 01:42:52 +0530

    Q46

---
+++ CODEQ46.c
@@ -0,0 +1,12 @@
+//Q46: Write a program to print the  pattern:
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= 5; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
