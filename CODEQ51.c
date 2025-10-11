318c394951b4fec984180d2db4fbeca9af726ad9
REPOSITARY <21arnavsaini@gmail.com>
2025-10-11 20:29:08 +0530

    Q51

---
+++ CODEQ51.c
@@ -0,0 +1,17 @@
+//Q51: Write a program to print the following pattern:
+  
+#include <stdio.h>
+
+int main() {
+    int i, j, space;
+    for(i = 5; i >= 1; i--) {
+        for(space = 1; space < i; space++) {
+            printf(" ");
+        }
+        for(j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
