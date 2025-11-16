ed625bb0624b409eccf97c5facdc3af614d7743d
REPOSITARY <21arnavsaini@gmail.com>
2025-11-16 22:22:43 +0530

    Q93

---
+++ CODEQ93.c
@@ -0,0 +1,32 @@
+//Q93: Check if two strings are anagrams of each other.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str1[100], str2[100];
+    int count[256] = {0}, i;
+
+    printf("Enter string 1: ");
+    scanf("%s", str1);
+    printf("Enter string 2: ");
+    scanf("%s", str2);
+
+    if (strlen(str1) != strlen(str2)) {
+        printf("Not anagrams\n");
+        return 0;
+    }
+
+    for (i = 0; str1[i] != '\0'; i++) {
+        count[(unsigned char)str1[i]]++;
+        count[(unsigned char)str2[i]]--;
+    }
+
+    for (i = 0; i < 256; i++) {
+        if (count[i] != 0) {
+            printf("Not anagrams\n");
+            return 0;
+        }
+    }
+    printf("Anagrams\n");
+    return 0;
+}
